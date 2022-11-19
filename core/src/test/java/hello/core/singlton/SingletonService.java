1e349c4d82b121d97ba4bb950b0f19177f709a10
superscman <vdcf2000@gmail.com>
2022-11-20 01:35:10 +0900

    스프링 기본편 공부중!

--- core/src/test/java/hello/core/singlton/SingletonService.java
+++ core/src/test/java/hello/core/singlton/SingletonService.java
@@ -1,2 +1,18 @@
-package hello.core.singlton;public class SingletonService {
+package hello.core.singlton;
+
+public class SingletonService {
+
+    private static final SingletonService instance = new SingletonService();
+
+    public static SingletonService getInstance() {
+        return instance;
+    }
+
+    private SingletonService() {
+
+    }
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
 }
